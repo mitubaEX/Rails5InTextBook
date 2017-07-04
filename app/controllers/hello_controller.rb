023c9313195c4eb104f2a999394c3881c67701e2
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2017-07-04 23:41:52 +0900

    ２章まで終了したのでcommitする

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,12 @@
+class HelloController < ApplicationController
+    def index
+        render plain: 'こんにちは，世界！'
+    end
+    def view
+        @msg = 'こんにちは，世界世界'
+    end
+
+    def list
+        @books = Book.all
+    end
+end
